cc7549329a19d8a971ebf09da5786329742e910e
Alvaro Gonzalia <gonzaliaalvaro@gmail.com>
2025-04-16 18:49:14 -0300

    add food model, get and post endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import userRoutes from "./routes/user.routes";
+import foodRoutes from "./routes/food.routes";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -7,6 +8,7 @@ const app = express();
 
 app.use(express.json());
 app.use("/api/users", userRoutes);
+app.use("/api/food", foodRoutes);
 
 const PORT = 3000;
 app.listen(PORT, () => {
